efdb27f0ce05e6c068b9f50eae66ecc11d202943
asofyan <asofyan@f9f769bc-5237-0410-8e98-f7c3699f5e56>
2008-03-19 13:10:36 +0000

    dev dari ipbs

--- dev.php
+++ dev.php
@@ -1,12 +1,13 @@
-<a href="http://localhost/sisrama/index.php/master/region/listregion">List Region</a>
+<b>Master</b>
+<a href="http://localhost/sisrama/index.php/master/region/page">List Region</a>
 <a href="http://localhost/sisrama/index.php/testunit/tumaster/testregionmodel">(Test Unit Region)</a><br>
-<a href="http://localhost/sisrama/index.php/master/campus/listcampus">List Campus</a>
+<a href="http://localhost/sisrama/index.php/master/campus/page">List Campus</a>
 <a href="http://localhost/sisrama/index.php/testunit/tumaster/testcampusmodel">(Test Unit Campus)</a><br>
-<a href="http://localhost/sisrama/index.php/master/periods/listperiods">List Periods</a>
-<a href="http://localhost/sisrama/index.php/testunit/tumaster/testperiodsmodel">(Test Unit Periods)</a><br>
-<a href="http://localhost/sisrama/index.php/master/students/liststudents">List Students</a>
-<a href="http://localhost/sisrama/index.php/testunit/tumaster/teststudentsmodel">(Test Unit Students)</a><br>
-<a href="http://localhost/sisrama/index.php/master/rooms/listrooms">List Rooms</a>
-<a href="http://localhost/sisrama/index.php/testunit/tumaster/testroomsmodel">(Test Unit Rooms)</a><br>
-<a href="http://localhost/sisrama/index.php/master/studentroom/liststudentroom">List StudentRoom</a>
-<a href="http://localhost/sisrama/index.php/testunit/tumaster/teststudentroommodel">(Test Unit StudentRoom)</a><br>
+<a href="http://localhost/sisrama/index.php/master/period/page">List Periods</a>
+<a href="http://localhost/sisrama/index.php/testunit/tumaster/testperiodmodel">(Test Unit Periods)</a><br>
+<a href="http://localhost/sisrama/index.php/master/student/page">List Students</a>
+<a href="http://localhost/sisrama/index.php/testunit/tumaster/teststudentmodel">(Test Unit Students)</a><br>
+<a href="http://localhost/sisrama/index.php/master/room/page">List Rooms</a>
+<a href="http://localhost/sisrama/index.php/testunit/tumaster/testroommodel">(Test Unit Rooms)</a><br>
+<a href="http://localhost/sisrama/index.php/master/studentroom/page">List StudentRoom</a>
+<!--<a href="http://localhost/sisrama/index.php/testunit/tumaster/teststudentroommodel">(Test Unit StudentRoom)</a><br>-->
